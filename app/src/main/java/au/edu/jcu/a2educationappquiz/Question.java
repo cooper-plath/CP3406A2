d21d6b5a26ae06942dfd6165a94802b745f1466c
Cooper Plath <cooper.plath@my.jcu.edu.au>
2022-04-21 12:47:52 +1000

    Finished game xml layout with radio groups and submit button, beginning on sqlite db

---
+++ app/src/main/java/au/edu/jcu/a2educationappquiz/Question.java
@@ -0,0 +1,9 @@
+package au.edu.jcu.a2educationappquiz;
+
+public class Question {
+    private String question;
+    private String option1;
+    private String option2;
+    private String option3;
+
+}
